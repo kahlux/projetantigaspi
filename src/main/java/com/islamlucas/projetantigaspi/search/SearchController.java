e627525de5552832aee937c32c2bb3eed012ab38
islamaboubakarovv <islamaboubakarov@gmail.com>
2022-05-14 21:56:42 +0200

    search suite
    search suite

--- src/main/java/com/islamlucas/projetantigaspi/search/SearchController.java
+++ src/main/java/com/islamlucas/projetantigaspi/search/SearchController.java
@@ -3,6 +3,7 @@ package com.islamlucas.projetantigaspi.search;
 import com.islamlucas.projetantigaspi.registration.RegistrationValidator;
 import com.islamlucas.projetantigaspi.registration.ShopValidator;
 import com.islamlucas.projetantigaspi.security.SecurityService;
+import com.islamlucas.projetantigaspi.shop.CartRepository;
 import com.islamlucas.projetantigaspi.shop.CategoryRepository;
 import com.islamlucas.projetantigaspi.shop.Shop;
 import com.islamlucas.projetantigaspi.users.User;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.islamlucas.projetantigaspi.users.UserRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -27,14 +29,20 @@ public class SearchController {
     private final RegistrationValidator registrationValidator;
     private final CategoryRepository categoryRepository;
     private final ShopValidator shopValidator;
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
 
 
     //search form traitement
     //fin du traitement search form
     @GetMapping("/search")
-    public String search(@RequestParam("q") String txt, HttpServletRequest request) {
+    public String search(@RequestParam("q") String q,Model model, HttpServletRequest request) {
         //trouver toutes les entités liées au texte saisi
-
+        String result="";
+        String user="";
+        String shop="";
+        String cart="";
+        model.addAttribute("result",result);
 
         return "search";
     }
